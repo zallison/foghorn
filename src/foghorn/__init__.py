47c176947ed2408797eba409dcf19de47ab531d7
Zack Allison <zack@zackallison.com>
2025-12-29 17:21:02 -0500

    chore(package): added __init__.py to make it a "real" package
    Brief: Marks `foghorn` as a regular package so entry points like `foghorn.main:main` work when installed from wheels/SDists.

---
+++ src/foghorn/__init__.py
@@ -0,0 +1,11 @@
+"""Foghorn package root.
+
+Brief: Marks `foghorn` as a regular package so entry points like
+`foghorn.main:main` work when installed from wheels/SDists.
+
+Inputs:
+  - None
+
+Outputs:
+  - None
+"""
